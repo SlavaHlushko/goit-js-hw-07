c55f32db6c1afd597cba0d6088b8bac9e3f86b89
SlavaHlushko <slavahlushko90@gmail.com>
2022-09-18 12:28:06 +0300

    Task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,46 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery"); 
+function createMarkup(items) {
+    return items.map((item) => `<li>
+<a class="gallery__item" href="large-image.jpg">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" 
+  data-source="${item.original}" />
+</a>
+</li>`).join("");
+    
+}
+
+const addMarkup = createMarkup(galleryItems);
+
+galleryRef.innerHTML = addMarkup;
+galleryRef.addEventListener("click", onImgClick);
+
+function onImgClick(event) {
+    blockAction(event);
+
+    if (event.target.nodeName !== "IMG") {
+        return
+    }
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captioDelay: 250,
+        captionsData: 'alt',
+    });
+
+    const instance = lightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+
+instance.show()
+
+    galleryRef.addEventListener("keydown", (event) => {
+        if (event.code === "Escape") {
+            instance.close()
+        }
+    });
+}
+function blockAction(event) {
+    event.preventDefault();
+}
